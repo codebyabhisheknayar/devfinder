c1ad72338428a1b50c16f22e353e00a850968833
Abhishek Faya <abhishek.sk@fayausa.com>
2024-02-24 22:43:48 +0530

    feat(core): update build file

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const isGithubActions = process.env.ACTIONS || false;
+const repo = isGithubActions ? process.env.REPOSITORY.replace(/.*?\//, "") : "";
+
+const assetPrefix = isGithubActions ? `/${repo}/` : "";
+const basePath = isGithubActions ? `/${repo}` : "";
+
 const nextConfig = {
     output: 'export',
-    basePath: '/docs',
+    reactStrictMode: true,
+    assetPrefix: assetPrefix,
+    basePath: basePath,
     distDir: 'docs',
     images: {
         unoptimized: true,
