111eba6652b0e8312726a8afcba32c1336602e6f
Abhishek Faya <abhishek.sk@fayausa.com>
2024-02-25 00:39:20 +0530

    refactor(core): update config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@
 const nextConfig = {
     output: 'export',
     basePath: '/devfinder',
+    distDir: 'dist',
     images: {
         unoptimized: true,
         domains: ['avatars.githubusercontent.com'],
