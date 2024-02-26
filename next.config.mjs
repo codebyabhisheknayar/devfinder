4b957280cd03bed4706c8b7ff6bdb18d9b643292
Abhishek Faya <abhishek.sk@fayausa.com>
2024-02-26 14:51:39 +0530

    refactor(core): update build file

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
-
+const prod = process.env.NODE_ENV === 'production'
 const nextConfig = {
     output: 'export',
     distDir: 'docs',
-    assetPrefix: "https://codebyabhisheknayar.github.io/devfinder",
+    assetPrefix: prod ? 'https://codebyabhisheknayar.github.io/devfinder' : '',
     images: {
         unoptimized: true,
         domains: ['avatars.githubusercontent.com'],
